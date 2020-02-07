0b5f68dcf24e1d1783dc5a1991195c0d89379090
E.B Studios <myebstudios@gmail.com>
2020-02-07 12:10:16 +0000

    code simplified.

--- main.py
+++ main.py
@@ -12,14 +12,9 @@ ant2.speed(15)
 ant2.color('blue')
 
 for i in range(ant_moves):
-  ant1_walk = random.randint(1, 10)
-  ant2_walk = random.randint(1, 10)
 
-  ant1_turn = random.randint(-45,45)
-  ant2_turn = random.randint(-45,45)
+  ant1.forward(random.randint(1, 10))
+  ant1.right(random.randint(-45,45))
 
-  ant1.forward(ant1_walk)
-  ant1.right(ant1_turn)
-
-  ant2.forward(ant2_walk)
-  ant2.right(ant2_turn)
+  ant2.forward(random.randint(1, 10))
+  ant2.right(random.randint(-45,45))
